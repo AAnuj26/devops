658148c89c75abaeea17fe377c8efdda468f5369
AAnuj26 <bhagatanuj2828@gmail.com>
2024-08-22 09:26:24 +0530

    chore: Add csurf middleware for CSRF protection

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 import express from "express";
-import helmet from "helmet";
 import cors from "cors";
+import helmet from "helmet";
+import csurf from "csurf";
 
 const app = express();
 
 app.use(helmet());
 
 app.use(cors());
+
+app.use(csurf());
 
 app.get("/", function (req, res) {
   return res.json({ message: "Hello World" });
