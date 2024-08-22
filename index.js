05987d6ed16a67b643a8b7945ad98d712a4e657e
AAnuj26 <bhagatanuj2828@gmail.com>
2024-08-22 09:14:45 +0530

    chore: Add helmet middleware for improved security

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 import express from "express";
+import helmet from "helmet";
 import cors from "cors";
 
 const app = express();
 
+app.use(helmet());
+
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("/", function (req, res) {
   return res.json({ message: "Hello World" });
 });
 
-app.listen(3000, () => {
+app.listen(3000, function () {
   console.log("Server is running on port 3000");
 });
